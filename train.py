632d0a418945d68ecd4946e4d6a3d9985bfb025e
Rakesh Biswas <rbiswas01999@gmail.com>
2024-12-27 23:52:44 +0600

    Train-Test splitted

--- train.py
+++ train.py
@@ -58,3 +58,7 @@ preprocessor = ColumnTransformer(
         ('cat', categorical_transformer, object_columns)
     ])
 
+# Train-Test Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+print("Train Size:", X_train.shape)
+print("Test Size:", X_test.shape)
